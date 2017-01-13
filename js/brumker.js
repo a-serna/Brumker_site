8e2a20972fd915f0babae3967fd9cbddfcd5e19c
a-serna <alejo.tanatos@gmail.com>
2017-01-12 23:46:27 -0500

    modify img portfolio section

--- js/brumker.js
+++ js/brumker.js
@@ -38,13 +38,13 @@ $( document ).ready(function(){
   // });
   $(function() {
   var selectedClass = "";
-  $(".fil-cat").click(function(){
-  selectedClass = $(this).attr("data-rel");
-   $("#portfolio").fadeTo(100, 0.1);
-  $("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+  $(".proyectos-selectores").click(function(){
+  selectedClass = $(this).attr("id");
+   $(".block-items").fadeTo(100, 0.1);
+  $(".block-items div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
   setTimeout(function() {
     $("."+selectedClass).fadeIn().addClass('scale-anm');
-    $("#portfolio").fadeTo(300, 1);
+    $(".block-items").fadeTo(300, 1);
   }, 300);
 
     });
