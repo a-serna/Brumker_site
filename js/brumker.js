92a6b41ccce733a8ad893d17d79ea60ca8720bd9
a-serna <alejo.tanatos@gmail.com>
2017-01-12 21:11:08 -0500

    modify filter portfolio

--- js/brumker.js
+++ js/brumker.js
@@ -21,19 +21,32 @@ $( document ).ready(function(){
 		});
 	});
   //filter portafolio
-  $('.proyectos-selectores').click(function(){
-    var proyectos = $(this).attr('id');
+  // $('.proyectos-selectores').click(function(){
+  //   var proyectos = $(this).attr('id');
+  //
+  //   if (proyectos == 'all'){
+  //     $('.proyectos-items').addClass('hide')
+  //     setTimeout(function(){
+  //       $('.proyectos-items').removeClass('hide');
+  //     }, 1000);
+  //   }else {
+  //     $('.proyectos-items').addClass('hide');
+  //     setTimeout(function(){
+  //       $('.' + proyectos).removeClass('hide');
+  //     }, 300);
+  //   }
+  // });
+  $(function() {
+  var selectedClass = "";
+  $(".fil-cat").click(function(){
+  selectedClass = $(this).attr("data-rel");
+   $("#portfolio").fadeTo(100, 0.1);
+  $("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+  setTimeout(function() {
+    $("."+selectedClass).fadeIn().addClass('scale-anm');
+    $("#portfolio").fadeTo(300, 1);
+  }, 300);
 
-    if (proyectos == 'all'){
-      $('.proyectos-items').addClass('hide')
-      setTimeout(function(){
-        $('.proyectos-items').removeClass('hide');
-      }, 1000);
-    }else {
-      $('.proyectos-items').addClass('hide');
-      setTimeout(function(){
-        $('.' + proyectos).removeClass('hide');
-      }, 300);
-    }
+    });
   });
 })
