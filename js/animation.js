4709899c93f8c7a7efd42ed602b69d81d5e6f7c0
a-serna <alejo.tanatos@gmail.com>
2017-02-04 01:17:18 -0500

    agregue imagenes y texto a la sección de inmobiliarias y constructoras en el vr html

--- js/animation.js
+++ js/animation.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-  var scroll;
-  $(window).scroll(function() {
-    scroll = $(window).scrollTop();
-
-    if (scroll > 300) {
-      $("#sector").addClass('animated bounceInRight');
-    }
-
-  });
+  // var scroll;
+  // $(window).scroll(function() {
+  //   scroll = $(window).scrollTop();
+  //
+  //   if (scroll > 300) {
+  //     $("#sector").addClass('animated bounceInRight');
+  //   }
+  //
+  // });
 })
