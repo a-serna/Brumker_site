30f5f755121d0f0a6d19ef9e9a6775a0ac1547f7
a-serna <alejo.tanatos@gmail.com>
2017-02-04 19:34:37 -0500

    agregue estilos y formulario y service php para funcionalidad

---
+++ service/service.php
@@ -0,0 +1,22 @@
+<?php
+  $first_name = $_POST['first_name'];
+  $enterprise = $_POST['enterprise'];
+  $email = $_POST['email'];
+  $service = $_POST['service'];
+  $budget = $_POST['budget'];
+  $budget_check = $_POST['budget_check'];
+  $mesage = $_POST['mesage'];
+  $from = 'From: www.brumker.com';
+  $to = 'info@brumker.com';
+  $subject = 'we want your service';
+
+  $body = "From: $first_name\n Enterprise: $enterprise\n E-Mail: $email\n Service: $service\n Budget: $budget\n Budget_check: $budget_check\n mesage:\n $mesage";
+
+  if ($_POST['submit']) {
+    if (mail ($to, $subject, $body, $from)) {
+         echo '<p>Tu mensaje ha sido enviado éxitosamente</p>';
+     } else {
+         echo '<p>Something went wrong, go back and try again!</p>';
+     }
+  }
+?>
