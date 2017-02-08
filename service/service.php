c86150c333dba6f1b9660ddbbdb12cd93575b6b6
a-serna <alejo.tanatos@gmail.com>
2017-02-07 21:43:26 -0500

    agregue tel y cel a los formularios de servicios

--- service/service.php
+++ service/service.php
@@ -3,6 +3,8 @@
   $enterprise = $_POST['enterprise'];
   $email = $_POST['email'];
   $service = $_POST['service'];
+  $tel = $_POST['tel'];
+  $cel = $_POST['cel'];
   $budget = $_POST['budget'];
   $budget_check = $_POST['budget_check'];
   $mesage = $_POST['mesage'];
@@ -10,7 +12,7 @@
   $to = 'info@brumker.com';
   $subject = 'we want your service';
 
-  $body = "From: $first_name\n Enterprise: $enterprise\n E-Mail: $email\n Service: $service\n Budget: $budget\n Budget_check: $budget_check\n mesage:\n $mesage";
+  $body = "From: $first_name\n Enterprise: $enterprise\n E-Mail: $email\n Service: $service\n Tel: $tel\n Cel: $cel\n Budget: $budget\n Budget_check: $budget_check\n mesage:\n $mesage";
 
   if ($_POST['submit']) {
     if (mail ($to, $subject, $body, $from)) {
