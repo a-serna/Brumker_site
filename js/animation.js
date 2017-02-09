421a8cb0856039a0dac0c74fe572a8fe972a1935
a-serna <alejo.tanatos@gmail.com>
2017-02-09 18:45:39 -0500

    arreglando problemas de color fondo

--- js/animation.js
+++ js/animation.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 
     if (scroll > 200) {
       $(".text").addClass('animated fadeInLeftBig').css({"opacity":"1"});
-      $("#item-list").addClass('animated fadeIn').css({"opacity":"1"});
+      $(".item-list").addClass('animated fadeIn').css({"opacity":"1"});
       $("#img-ach").addClass('animated hinge bounceInRight').css({"opacity":"1"});
       $("#img-man").addClass('animated hinge slideInUp').css({"opacity":"1"});
       $("#img-web").addClass('animated hinge slideInUp').css({"opacity":"1"});
@@ -17,13 +17,13 @@ $(document).ready(function(){
     };
     if (scroll > 650) {
       $(".text-2").addClass('animated fadeInRightBig').css({"opacity":"1"});
-      $("#item-list-2").addClass('animated fadeIn').css({"opacity":"1"});
+      $(".item-list-2").addClass('animated fadeIn').css({"opacity":"1"});
       $("#img-interactive").addClass('animated hinge bounceInLeft').css({"opacity":"1"});
       $("#cotizar-2").addClass('animated hinge fadeIn').css({"opacity":"1"});
     };
     if (scroll > 900) {
       $(".text-3").addClass('animated fadeInLeftBig').css({"opacity":"1"});
-      $("#item-list-3").addClass('animated hinge fadeIn').css({"opacity":"1"});
+      $(".item-list-3").addClass('animated hinge fadeIn').css({"opacity":"1"});
       $("#cotizar-3").addClass('animated hinge fadeIn').css({"opacity":"1"});
       $("#img-app").addClass('animated hinge fadeInRightBig').css({"opacity":"1"});
     }
