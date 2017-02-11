c14ae7f86e0e5abf335e721ec8201750a34f8f34
a-serna <alejo.tanatos@gmail.com>
2017-02-11 01:17:00 -0500

    3d html terminado con responsive

--- js/animation.js
+++ js/animation.js
@@ -19,6 +19,7 @@ $(document).ready(function(){
     if (scroll > 650) {
       $(".text-2").addClass('animated fadeInRightBig').css({"opacity":"1"});
       $(".item-list-2").addClass('animated fadeIn').css({"opacity":"1"});
+      $(".item-list-4").addClass('animated fadeIn').css({"opacity":"1"});
       $("#img-interactive").addClass('animated hinge bounceInLeft').css({"opacity":"1"});
       $("#img-producto").addClass('animated hinge bounceInLeft').css({"opacity":"1"});
       $("#cotizar-2").addClass('animated hinge fadeIn').css({"opacity":"1"});
